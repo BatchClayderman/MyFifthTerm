5a9e5092dbf2f9dac50066dc311986c39bb13cfb
BatchClayderman <57588843+BatchClayderman@users.noreply.github.com>
2022-07-23 12:36:04 +0800

    Add files via upload

---
+++ BFSPack/BFSPack/BFSPack.cpp
@@ -0,0 +1,204 @@
+﻿#include <iostream>
+#include <string>
+#include <queue>
+using namespace std;
+const int max_num = 64;//最大背包数量的常量
+
+
+struct object//物品结构体
+{
+	double w, v, d;//重量、价值、价值重量比
+	int id;//物品序号
+} a[max_num];
+int cnt, best_vector_x[max_num];//物品种数和最优解向量
+double weights, bestValue;//背包容量和最优价值
+
+struct node//结点结构体
+{
+	double cv, lv, lw;//当前价值、价值上界和剩余容量
+	int id, x[max_num];//物品排序后的序号和解向量
+	node()
+	{
+		memset(x, 0, sizeof(x));
+		lv = 0;
+	}
+	node(double _cv, double _lv, double _lw, int _id)//构造函数
+		:cv(_cv), lv(_lv), lw(_lw), id(_id)
+	{
+		memset(x, 0, sizeof(x));
+	}
+};
+
+struct NodeCompare//结点优先级，价值上界高的优先级高
+{
+	bool operator() (node& n1, node& n2)
+	{
+		return n1.lv < n2.lv;
+	}
+};
+
+
+bool compareBetween(object b1, object b2)//以单位价值高的优先
+{
+	return b1.d > b2.d;
+}
+
+void init()//初始化
+{
+	memset(best_vector_x, 0, sizeof(best_vector_x));
+	bestValue = 0;
+	sort(a + 1, a + cnt + 1, compareBetween);//以 compareBetween 的比较方法排序
+	return;
+}
+
+double calcInf(node t)//计算价值上界
+{
+	int num = t.id;
+	double maxvalue = t.cv, left = t.lw;
+	while (num <= cnt && left >= a[num].w)
+	{
+		left -= a[num].w;
+		maxvalue += a[num].v;
+		++num;
+	}
+	if (num <= cnt && left > 0)//记得防止越界
+		maxvalue += 1.0 * a[num].v / a[num].w * left;// 1.0 乘可以转 double
+	return maxvalue;
+}
+
+void bfs()
+{
+	priority_queue<node, vector<node>, NodeCompare> q;
+	double sumv = 0;
+	for (int i = 1; i <= cnt; ++i)
+		sumv += a[i].v;
+	q.push(node(0, sumv, weights, 1));//放入根结点
+
+	while (!q.empty())
+	{
+		node live, lchild, rchild;
+		live = q.top();
+		q.pop();
+		int t = live.id;//当前处理物品的序号
+		if (t > cnt || live.lw == 0)//到达叶子结点或者没有容量了
+		{
+			if (live.cv >= bestValue)//更新最优解（第一次计算得到的值也要更新）
+			{
+				for (int i = 1; i <= cnt; ++i)
+					best_vector_x[i] = live.x[i];
+				bestValue = live.cv;
+			}
+			continue;
+		}
+
+		if (live.lv < bestValue)//不满足限界条件
+			continue;
+
+		if (live.lw >= a[t].w)//满足约束条件，可以生成左孩子
+		{
+			lchild.cv = live.cv + a[t].v;
+			lchild.lw = live.lw - a[t].w;
+			lchild.id = t + 1;
+			lchild.lv = calcInf(lchild);
+			for (int i = 1; i <= cnt; ++i)
+				lchild.x[i] = live.x[i];
+			lchild.x[t] = 1;
+			if (lchild.cv > bestValue)//注意要更新最优值
+				bestValue = lchild.cv;
+			q.push(lchild);
+		}
+
+		rchild.cv = live.cv;
+		rchild.lw = live.lw;
+		rchild.id = t + 1;
+		rchild.lv = calcInf(rchild);
+		if (rchild.lv >= bestValue)//满足限界条件，可以生成右孩子
+		{
+			for (int i = 1; i <= cnt; ++i)
+				rchild.x[i] = live.x[i];
+			rchild.x[t] = 0;
+			q.push(rchild);
+		}
+	}
+	return;
+}
+
+void rfstdin()
+{
+	rewind(stdin);
+	fflush(stdin);
+	return;
+}
+
+void input(string input_str, double& w, double& v)
+{
+	size_t index = 0;
+	for (index = 0; index < input_str.size(); ++index)//查找空格所在的地方
+		if (' ' == input_str[index] || '\t' == input_str[index])
+			break;
+	if (index <= 0 || index >= input_str.size() - 1)//没有找到合适的分隔符号
+	{
+		w = NULL;
+		v = NULL;
+	}
+	else
+	{
+		w = atof(input_str.substr(0, index).c_str());
+		v = atof(input_str.substr(index + 1).c_str());
+	}
+	return;
+}
+
+void output()
+{
+	cout << endl << "可装载物品的最大价值为：" << bestValue << endl;
+	if (bestValue > 0)
+	{
+		cout << "装入的物品 ID 为：" << endl;
+		cout << "{" << a[1].id;
+		for (int i = 2; i <= cnt; ++i)
+			if (best_vector_x[i])
+				cout << ", " << a[i].id;
+		cout << "}" << endl;
+	}
+	return;
+}
+
+
+
+int main()
+{
+	cout << "请输入物品个数：";
+	rfstdin();
+	cin >> cnt;
+	cout << "请输入背包容量：";
+	rfstdin();
+	cin >> weights;
+	if (cnt < 1 || weights < 1 || cnt > max_num)
+	{
+		cout << endl << "\a您的输入不合理或超出问题处理规模限制，程序已退出。" << endl;
+		return EXIT_FAILURE;
+	}
+
+	for (int i = 1; i <= cnt; ++i)
+	{
+		cout << "请输入物品 " << i << " 的重量和价值（使用一个空格隔开）：";
+		rfstdin();
+		string input_str;
+		getline(cin, input_str);
+		input(input_str, a[i].w, a[i].v);
+		if (a[i].w < 1 || a[i].v < 1)
+		{
+			--i;//重新输入
+			cout << "\a上述输入有误，请重新输入。" << endl;
+			continue;
+		}
+		a[i].d = a[i].v / a[i].w;
+		a[i].id = i;//标记 id
+	}
+
+	init();
+	bfs();
+	output();
+	return EXIT_SUCCESS;
+}
---
+++ minSpanningTree/minSpanningTree/minSpanningTree.cpp
@@ -0,0 +1,313 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <vector>
+#include <iomanip>
+#ifndef _MST_H
+#define _MST_H 2/* 1 = Prim   2 = Kruskal */
+#define INF 0xFFFFFF
+#define NodeUB 16
+#define NodeLB 2
+#define DistanceUB 512
+#define DistanceLB 1
+#define PromptLeft  "/****************************** "
+#define PromptRight " ******************************/"
+#define PromptWidth 6
+#define DATA "data.txt"
+#define SEPRATE ' '
+#endif//_MST_H
+using namespace std;
+typedef int DistanceType;
+DistanceType setPK[NodeUB];
+
+
+struct edge
+{
+	DistanceType x, y;
+	DistanceType w;
+};
+
+
+/* 从文件读入数据 */
+#ifdef WIN32
+string GF_GetEXEPath()//获取程序所在路径
+{
+	char FilePath[MAX_PATH];
+	GetModuleFileNameA(NULL, FilePath, MAX_PATH);
+	(strrchr(FilePath, '\\'))[1] = 0;
+	return string(FilePath);
+}
+#else
+string GF_GetEXEPath()
+{
+	int rval;
+	char link_target[4096];
+	char* last_slash;
+	size_t result_Length;
+	char* result;
+	string strExeDir;
+	rval = readlink("/proc/self/exe", link_target, 4096);
+	if (rval < 0 || rval >= 1024)
+		return "";
+	link_target[rval] = 0;
+	last_slash = strrchr(link_target, '/');
+	if (last_slash == 0 || last_slash == link_target)
+		return "";
+	result_Length = last_slash - link_target;
+	result = (char*)malloc(result_Length + 1);
+	strncpy(result, link_target, result_Length);
+	result[result_Length] = 0;
+	strExeDir.append(result);
+	strExeDir.append("/");
+	free(result);
+	return strExeDir;
+}
+#endif
+
+vector<DistanceType> split(string strArray, char seprate)//按行转 vector
+{
+	char lastChar = seprate;
+	string s = "";
+	for (size_t i = 0; i < strArray.size(); ++i)
+		if ((strArray[i] >= '0' && strArray[i] <= '9') || '.' == strArray[i] || '-' == strArray[i])//合法的值（连续小数点由 atof 处理）
+		{
+			s += strArray[i];
+			lastChar = strArray[i];
+		}
+		else if (lastChar != seprate)//任意非法字符作为分隔符并去重
+		{
+			s += seprate;
+			lastChar = seprate;
+		}
+	if (s.length() <= 0)//空输入置零
+		s = "0";
+
+	vector<DistanceType> ret;
+	size_t seprate_len = 1, start = 0, index = 0;
+	while ((index = s.find(seprate, start)) != -1)
+	{
+		if (s.substr(start, index - start) == "-1" || s.substr(start, index - start) == "INF")//此路不通
+			ret.push_back(INF);
+		else
+			ret.push_back((DistanceType)atof(s.substr(start, index - start).c_str()));
+		start = index + seprate_len;
+	}
+	if (start < s.length())
+	{
+		if (s.substr(start, s.length() - start) == "-1" || s.substr(start, s.length() - start) == "INF")
+			ret.push_back(INF);
+		else
+			ret.push_back((DistanceType)atof(s.substr(start, s.length() - start).c_str()));
+	}
+	return ret;
+}
+
+bool getInput(vector<DistanceType>& G, int& n)
+{
+	FILE* fp;
+	fopen_s(&fp, (GF_GetEXEPath() + DATA).c_str(), "r");//打开目标文件
+	if (nullptr != fp)
+	{
+		string tmpString = "";//存放每行
+		for (;;)//无条件死循环
+		{
+			char c = fgetc(fp);//逐字符遍历
+			if (EOF == c)//如果遍历完成
+			{
+				if (tmpString.length() > 0)
+				{
+					vector<DistanceType> tmp = split(tmpString, SEPRATE);//将现有的 tmpString 一并放入图中
+					for (size_t i = 0; i < tmp.size(); ++i)
+						G.push_back(tmp[i]);
+				}
+				break;
+			}
+			else if ('\n' == c)//换行
+			{
+				vector<DistanceType> tmp = split(tmpString, SEPRATE);
+				for (size_t i = 0; i < tmp.size(); ++i)
+					G.push_back(tmp[i]);
+				tmpString = "";//记得清行
+			}
+			else
+				tmpString += c;
+		}
+		if (sqrt(G.size()) == (int)sqrt(G.size()))
+		{
+			n = (int)sqrt(G.size());
+			return true;
+		}
+		else
+			return false;
+	}
+	else
+		return false;
+}
+
+#if (1 == _MST_H)
+/* Prim */
+void Prim(vector<DistanceType>& A, int n)
+{
+	vector<DistanceType> prev, cost;
+	for (int i = 0; i < n; ++i)
+	{
+		prev.push_back(NULL);
+		cost.push_back(INF);
+		setPK[i] = 0;
+	}
+	int ori_min = INF, new_min = NULL, count = 0;
+	cost[0] = 0;// set 0 as root
+	prev[0] = -1;
+
+	while (count < n - 1)
+	{
+		for (int i = 0; i < n; ++i)
+			if (setPK[i] == 0 && cost[i] < ori_min)
+			{
+				ori_min = cost[i];
+				new_min = i;
+			}
+		
+		setPK[new_min] = 1;// set min1 as next node 
+		for (int i = 0; i < n; ++i)
+		{
+			if (A[new_min * n + i] > 0 && cost[i] > A[new_min * n + i] && setPK[i] == 0)
+			{
+				cost[i] = A[new_min * n + i];
+				prev[i] = new_min;
+			}
+		}
+		ori_min = INF;
+		++count;
+	}
+
+	cout << PromptLeft << "Prim Minimum Spanning Tree" << PromptRight << endl;
+	for (int i = 1; i < n; ++i)
+		cout << "\t\tEdge: " << setw(2) << prev[i] << " - " << setw(2) << i << "\t\t\tWeight: " << A[prev[i] * n + i] << endl;
+	return;
+}
+
+#elif (2 == _MST_H)
+/* Kruskal */
+void sort(vector<edge>& a, int n)// simply use bubble
+{
+	for (int i = 0; i < n - 1; ++i)
+		for (int j = i + 1; j < n; ++j)
+			if (a[i].w > a[j].w)
+				swap(a[i], a[j]);
+	return;
+}
+
+int findK(int x)
+{
+	int root = x, node = x, parent;
+	while (root != setPK[root])
+		root = setPK[root];
+	while (node != root)
+	{
+		parent = setPK[node];
+		setPK[node] = root;
+		node = parent;
+	}
+	return root;
+}
+
+void unionK(vector<DistanceType>& height, int repx, int repy)// remember to use &
+{
+	if (height[repx] == height[repy])
+	{
+		++height[repx];
+		setPK[repy] = repx;// y's tree points to x's tree
+	}
+	else if (height[repx] > height[repy])
+		setPK[repy] = repx;// y's tree points to x's tree
+	else
+		setPK[repx] = repy;// x's tree points to y's tree
+}
+
+void Kruskal(vector<DistanceType> &A, int n)
+{
+	vector<edge> E, T;
+	vector<DistanceType> height;
+	edge tmp = { 0, 0, 0 };
+	for (int i = 0; i < n; ++i)
+	{
+		height.push_back(NULL);
+		for (int j = 0; j < n; ++j)
+		{
+			E.push_back({ A[i * n + j], i, j });
+			T.push_back(tmp);
+		}
+	}
+	
+	sort(E, n * n);// Sort the edge firstly
+
+	for (int i = 0; i < n; ++i)// Make a set
+	{
+		setPK[i] = i;
+		height[i] = 0;
+	}
+
+	int index = 0;
+	for (int i = 0; i < n * n; ++i)
+		if (E[i].w != 0)
+		{
+			int ucomp = findK(E[i].x), vcomp = findK(E[i].y);
+			if (vcomp != ucomp)
+			{
+				T[index] = E[i];
+				unionK(height, ucomp, vcomp);
+				if (++index == n - 1)
+					break;
+			}
+		}
+	
+	cout << PromptLeft << "Kruskal Minimum Spanning Tree" << PromptRight << endl;
+	for (int i = 0; i < n - 1; ++i)
+		cout << "\t\tEdge: " << setw(2) << T[i].x << " - " << setw(2) << T[i].y << "\t\t\tWeight: " << T[i].w << endl;
+	return;
+}
+#endif
+
+
+
+int main()
+{
+	/* preparation */
+	vector<DistanceType> A;
+	srand((unsigned int)time(NULL));
+	int n = (rand() % (NodeUB - NodeLB + 1)) + NodeLB;// rang in [NodeLB, NodeUB]
+	
+	/* initial */
+	if (getInput(A, n))
+		cout << "Initial: " << n << " vertices read from " << DATA << ". " << endl << endl;
+	else
+	{
+		cout << "Initial: " << n << " vertices rang in [" << NodeLB << ", " << NodeUB << "]. " << endl << endl;
+		for (int i = 0; i < n; ++i)
+			for (int j = 0; j < n; ++j)
+				if (i < j)
+					A.push_back(rand() % (DistanceUB - DistanceLB + 1) + DistanceLB);
+				else if (i == j)
+					A.push_back(NULL);
+				else
+					A.push_back(A[j * n + i]);
+	}
+
+	/* output */
+	cout << PromptLeft << "Two-Dimensional Matrix A (" << n << " * " << n << ")" << PromptRight << endl;
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+			cout << setw(PromptWidth) << A[i * n + j];
+		cout << endl;
+	}
+	cout << endl;
+
+#if (1 == _MST_H)
+	Prim(A, n);
+#elif (2 == _MST_H)
+	Kruskal(A, n);
+#endif
+	cout << endl << endl;
+	return EXIT_SUCCESS;
+}
